673d28c2e81028b8564cc6ab1716596a01e9f66d
Shown Shaju <shownshaju@gamil.com>
2022-03-10 12:39:04 +0530

    h1 chaneg updated with the winner announcement

--- DOM/Dicee Challenge - Starting Files/index.js
+++ DOM/Dicee Challenge - Starting Files/index.js
@@ -19,3 +19,21 @@ var randomImageSource2 ="images/" + randomDiceImage2;
 var img2 =document.querySelectorAll("img")[1];
 
 img2.setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="🚩 Player 1 Wins";
+
+}
+
+else if(randomNumber2 > randomNumber1)
+{
+
+    document.querySelector("h1").innerHTML="🚩 Player 2 Wins";
+}
+
+else 
+{
+    document.querySelector("h1").innerHTML="Draw";
+}
