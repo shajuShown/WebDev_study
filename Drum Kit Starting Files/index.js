4e348dd085b1d55f9900490d994f2f50feed9bb8
Shown Shaju <shownshaju@gamil.com>
2022-03-28 14:44:24 +0530

    button click function added to all buttons using for looop

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,8 +1,12 @@
 
 
-document.querySelector("button").addEventListener("click" ,handleClick)
+
+for(var i=0; i<document.querySelectorAll(".drum").length;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click" ,handleClick)
 
 
-function handleClick(){
-    alert("I got clicked!");
+    function handleClick(){
+        alert("I got clicked!");
+    }
 }
