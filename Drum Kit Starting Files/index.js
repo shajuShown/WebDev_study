15253acce32828a44651b21f0aaba093b0e10437
Shown Shaju <shownshaju@gamil.com>
2022-04-03 13:21:39 +0530

    animation added to the buttons

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -7,12 +7,14 @@ for(var i=0; i<document.querySelectorAll(".drum").length;i++)
         var buttonInnerHTML = this.innerHTML;
 
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML)
     });
 
 }
 // detecting keyboard press
 document.addEventListener("keypress", function(event){
     makeSound(event.key)
+    buttonAnimation(event.key)
 });
 
 
@@ -57,4 +59,16 @@ function makeSound(key){
          default: console.log(buttonInnerHTML)
 
 }
+}
+
+function buttonAnimation(currentkey)
+{
+   
+    var activeButton = document.querySelector("."+currentkey);
+    activeButton.classList.add("pressed")
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+
 }
