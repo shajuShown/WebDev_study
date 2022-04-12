8cb3e84aa77b6f012b2c6d7a4b42ce0c2bf7ef35
Shown Shaju <shownshaju@gamil.com>
2022-04-12 19:13:10 +0530

    Calculator challenge done

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,7 +1,9 @@
 //jshint esversion:6
 
 const express= require("express");
+const bodyParser =require("body-parser");
 const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 
@@ -10,5 +12,16 @@ app.listen(3000,function(){
 });
 
 app.get("/",function(req,res){
-    res.send("Hello World")
+    res.sendFile(__dirname +"/index.html");
+});
+
+
+app.post("/",function(req,res){
+
+
+
+    var num1=Number(req.body.num1);
+    var num2=Number(req.body.num2);
+    var result = num1+num2;
+res.send("The result of the calculationn is "+ result)
 });
