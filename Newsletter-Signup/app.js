6c49db8cc1ff36de6dd0d92cadb2e1ff92ba6a57
Shown Shaju <shownshaju@gamil.com>
2022-04-19 22:13:21 +0530

    Bootstrap template  added- link appended , css file created

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const request = require("request")
+
+const app = express();
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 
