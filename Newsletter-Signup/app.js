89fe578007321b7b05aec54467ec8b0ee14d31b9
Shown Shaju <shownshaju@gamil.com>
2022-04-19 22:52:51 +0530

    login page setup done get and post implementteda nd tested

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request")
+
 
 const app = express();
 
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.post("/", function(req,res){
+
+const firstName=req.body.fname;
+const lastName=req.body.lname;
+const email=req.body.email;
+
+console.log(firstName,lastName,email);
+
+});
+
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
 
 
 
