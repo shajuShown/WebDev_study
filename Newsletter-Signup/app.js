e69bf84b33be1b44a8f046d114c9b1bad77671a9
Shown Shaju <shownshaju@gamil.com>
2022-04-20 10:09:26 +0530

    not working but still uploading on heroku

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
+const request = require("request")
 
 
 const app = express();
@@ -10,11 +12,49 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.post("/", function(req,res){
 
-const firstName=req.body.fname;
-const lastName=req.body.lname;
-const email=req.body.email;
+    const firstName=req.body.fname;
+    const lastName=req.body.lname;
+    const email=req.body.email;
 
-console.log(firstName,lastName,email);
+    const data ={
+        members:[
+        {
+            email_address: email,
+            status:"subscribed",
+            merge_fields:{
+                 FNAME:firstName,
+                 LNAME:lastName,
+            }
+
+        }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    console.log(jsonData)
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/bbb07e82bc";
+
+
+    const options ={
+        method: "POST",
+        auth: "ShownShaju2:23a5e9ce6723f0aae3d83b303e270c398-us14"
+    }
+    const request = https.request(url, options, function(response){
+
+        if (response.statusCode ===200){
+            res.send("Successfully subscribed!");
+        }
+        else{
+            res.send("There was an error with signing up, please try again")
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+        
+        request.write(jsonData);
+        request.end;
 
 });
 
@@ -25,7 +65,15 @@ app.get("/", function(req,res){
 
 
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Server is running on port 3000");
 
 });
+
+
+
+//API KEY
+//3a5e9ce6723f0aae3d83b303e270c398-us14
+
+//List Id
+//bbb07e82bc
